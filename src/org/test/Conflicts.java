9d46ae34531d8085e3df004e098c7ca94decc413
udhay007 <udhaysmart8@gmail.com>
2022-01-01 10:12:28 +0530

    conflict resolved

--- src/org/test/Conflicts.java
+++ src/org/test/Conflicts.java
@@ -1,5 +1,15 @@
 package org.test;
 
 public class Conflicts {
+	
+	public static void password() {
+		System.out.println("password");
+
+	}
+	public static void main(String[] args) {
+		password();
+		
+		
+	}
 
 }
