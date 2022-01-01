0a287b75060c0de7008dfce8ba86283576f09b7d
udhay007 <udhaysmart8@gmail.com>
2022-01-01 10:24:43 +0530

    conflict resolveed

--- src/org/test/Conflicts.java
+++ src/org/test/Conflicts.java
@@ -1,5 +1,13 @@
 package org.test;
 
 public class Conflicts {
+	
+	public static void username() {
+		System.out.println("username");
+
+	}
+	public static void main(String[] args) {
+		username();
+	}
 
 }
